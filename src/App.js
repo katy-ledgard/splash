049f618d5e355ee4fea3183e4424f6f1e62054f2
Katy <katy.ledgard@gmail.com>
2023-07-26 15:53:39 +0100

    splash front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [img, setImg] = useState("");
+
+  function handleSearch(event) {
+    //console.log("always test to check function is working");
+    setSearchQuery(event.target.value);
+    // console.log(searchQuery) <- check it is updating state
+  }
+
+  //This function needs to take time to get the required data; this is why we set it as an async function and use await. That means the rest of the code won't run until they get the response they have requested.
+
+  async function getImage() {
+    try {
+    const API = `http://localhost:8090/photos?subject=${searchQuery}`;
+    const res = await axios.get(API);
+    console.log(res);
+    setImg(res.data[0].img_url);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find any image</h1>
+      
+        <input
+          type="text"
+          placeholder="enter image subject"
+          onChange={handleSearch}
+        />
+        <button onClick={getImage}>Explore!</button>
+        {img && <img src={img} alt={searchQuery} />}
+      
     </div>
   );
 }
